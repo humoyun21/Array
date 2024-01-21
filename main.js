08d2fb358c6a04569b1e00551b70498f26e570a0
humoyun21 <husanovhumoyunbek20052109@gmail.com>
2024-01-22 00:40:44 +0500

    array

---
+++ main.js
@@ -0,0 +1,91 @@
+/*let str ="Jonajon vatanim O'zbekiston"
+    
+let len =str.length
+    console.log(len)
+*/
+/*
+
+let str = "Jonajon vatanim O'zbekiston";
+console.log(str.charCodeAt());
+*/
+
+/*
+
+let str = "Jonajon vatanim O'zbekiston";
+let str1=str.slice(1,7)
+console.log(str1);
+
+*/
+
+/*
+let str = "Jonajon vatanim O'zbekiston";
+let str1 = str.slice(-1, 15);
+console.log(str1);
+*/
+
+/*
+let str = "Jonajon vatanim O'zbekiston";
+let str1 = str.substring(1, 3);
+console.log(str1);
+
+*/
+
+/*
+
+let str = "Jonajon vatanim O'zbekiston";
+let str1 = "emas"
+let a=str.concat(str1)
+console.log(a);
+
+*/
+
+/*
+
+let str = "Jonajon vatanim O'zbekiston";
+str.padStart(4 , "b") 
+console.log(str);
+
+*/
+
+/*
+
+let str = "Jonajon vatanim O'zbekiston";
+console.log(str.padStart(4, "b"));
+
+*/
+
+/*
+let str = "7";
+console.log(str.padStart(7, "1"));
+*/
+
+/*
+let str = "Jonajon vatanim O'zbekiston";
+console.log(str.replace("jon", "bek"));
+*/
+/*
+let str = "Jonajon vatanim O'zbekiston";
+console.log(str.replace("t", "b"));
+*/
+
+/*
+
+let str="abcd"
+
+console.log(str.replaceAll("" , " ").trim());
+*/
+/*
+let str = "abcd";
+
+console.log(str.split("", " ").reverse().join("harfi"));
+*/
+/*
+let str = "abcd";
+
+console.log(str.split("").reverse().join(""));
+    let str1 ="Toshkenta Havo yaxshi"
+    for (let i = 0; i < str.length; i++) {
+        console.log(str[i]);
+        
+    }
+*/
